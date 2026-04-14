0fa08479b1af009f98315e8f81aaac09a81ca13f
Stepan <stepan.fomin105@gmail.com>
2026-04-14 16:20:55 +0300

    Настроил линтеры

--- lib/core/router/router_provider.dart
+++ lib/core/router/router_provider.dart
@@ -1,9 +1,8 @@
+import 'package:base_flutter_proj/core/router/router_shell.dart';
 import 'package:base_flutter_proj/home/home_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
-
-import 'router_shell.dart';
 
 final goRouterProvider = Provider<GoRouter>((ref) {
   return GoRouter(
